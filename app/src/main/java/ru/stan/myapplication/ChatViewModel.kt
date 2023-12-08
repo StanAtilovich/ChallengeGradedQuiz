c8190df99935b9bbe4ce60985930d9410cb3e6c5
StanAtilovich <alano92@yandex.ru>
2023-12-08 10:11:06 +0800

    Challenge: Tracking Cheat Status by Question

--- app/src/main/java/ru/stan/myapplication/ChatViewModel.kt
+++ app/src/main/java/ru/stan/myapplication/ChatViewModel.kt
@@ -2,7 +2,7 @@ package ru.stan.myapplication
 
 import androidx.lifecycle.ViewModel
 
-class ChatViewModel(): ViewModel() {
+class ChatViewModel: ViewModel() {
     var answerWasClicked = false
 
 }
